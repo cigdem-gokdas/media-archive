a21e99cb7d527968d6326293ec3c3360f43325fb
Başak Eriş <basakeris4@gmail.com>
2025-11-28 14:47:13 +0300

    searchmanagerduzeltme

--- search_manager.py
+++ search_manager.py
@@ -1,4 +1,4 @@
-# playwright
+#playwright
 from playwright.sync_api import sync_playwright
 
 
@@ -6,41 +6,46 @@ def find_link(movie_name):
     print(f"Searching for {movie_name}...")
     search_query = movie_name.replace(" ", "+")
 
-    # start playwright
-    with sync_playwright() as pw:
-
-        # launch browser
-        browser = pw.chromium.launch(headless=True)
-        # we show that we are not bots to enter IMDB
+    #start playwright
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=True)
+        #anti-bot headers
         context = browser.new_context(
-            user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"
+            user_agent=(
+                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+                "AppleWebKit/537.36 (KHTML, like Gecko)"
+                "Chrome/114.0.0.0 Safari/537.36"
+            )
         )
         page = context.new_page()
 
         try:
-
-            # navigate to the search page
+            #navigate to the search page
             page.goto(
-                f"https://www.imdb.com/find/?q={search_query}", wait_until="domcontentloaded", timeout=30000)
+                f"https://www.imdb.com/find/?q={search_query}",
+                wait_until="domcontentloaded",
+                timeout=30000
+            )
             selector = "ul.ipc-metadata-list li:first-child a"
             page.wait_for_selector(selector, timeout=20000)
 
             first_result = page.locator(selector).first
             href = first_result.get_attribute("href")
+            
             if not href:
                 print("no results.")
                 return None
 
             clean_link = href.split("?")[0]
-            link = "https://www.imdb.com" + clean_link
-            print(f"The link is found: {link}")
-            return link
+            final_link = "https://www.imdb.com" + clean_link
+            print(f"The link is found: {final_link}")
+            return final_link
 
-        except Exception as exception:
-            print(f"{movie_name} not found.Error: {exception}")
+        except Exception as error:
+            print(f"{movie_name} not found.Error: {error}")
 
             try:
-                print(f"Sayfa Başlığı: {page.title()}")
+                print(f"Page Title: {page.title()}")
             except:
                 pass
             return None
