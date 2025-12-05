3aa0f63aa1fb82a427d1a8beec6c0982c48ffef2
Başak Eriş <basakeris4@gmail.com>
2025-12-05 14:02:49 +0300

    searchmanager2

--- search_manager.py
+++ search_manager.py
@@ -1,53 +1,42 @@
-#playwright
 from playwright.sync_api import sync_playwright
-
 
 def find_link(movie_name):
     print(f"Searching for {movie_name}...")
-    search_query = movie_name.replace(" ", "+")
+    query = movie_name.replace(" ", "+")
 
-    #start playwright
     with sync_playwright() as p:
         browser = p.chromium.launch(headless=True)
-        #anti-bot headers
         context = browser.new_context(
             user_agent=(
                 "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
-                "AppleWebKit/537.36 (KHTML, like Gecko)"
+                "AppleWebKit/537.36 (KHTML, like Gecko) "
                 "Chrome/114.0.0.0 Safari/537.36"
             )
         )
         page = context.new_page()
 
         try:
-            #navigate to the search page
-            page.goto(
-                f"https://www.imdb.com/find/?q={search_query}",
-                wait_until="domcontentloaded",
-                timeout=30000
-            )
-            selector = "ul.ipc-metadata-list li:first-child a"
-            page.wait_for_selector(selector, timeout=20000)
+            page.goto(f"https://www.imdb.com/find/?q={query}", timeout=30000)
+
+            # SADECE "Titles" sonuçlarını al
+            selector = "section[data-testid='find-results-section-title'] li a"
+
+            page.wait_for_selector(selector, timeout=10000)
 
             first_result = page.locator(selector).first
             href = first_result.get_attribute("href")
-            
-            if not href:
-                print("no results.")
-                return None
 
-            clean_link = href.split("?")[0]
-            final_link = "https://www.imdb.com" + clean_link
-            print(f"The link is found: {final_link}")
-            return final_link
+            if href and "/title/" in href:
+                final = "https://www.imdb.com" + href.split("?")[0]
+                print("The link is found:", final)
+                return final
 
-        except Exception as error:
-            print(f"{movie_name} not found.Error: {error}")
-
-            try:
-                print(f"Page Title: {page.title()}")
-            except:
-                pass
+            print("No valid title result found.")
             return None
+
+        except Exception as e:
+            print("Error:", e)
+            return None
+
         finally:
             browser.close()
